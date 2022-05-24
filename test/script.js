a6000124b43d0ca10a1f4d0b44f85ac579fcfdb3
Mohamed Abdelhay <93220261+MohamedAbdelhay00@users.noreply.github.com>
2022-05-24 15:48:12 +0200

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,27 @@
+
+
+
+/*function multiplication(a, b) {
+  return a*b;
+}
+function showAlert(a, b) {
+  alert("يا الله كل يوم نفس الاشي");
+}
+
+function showConfirm(a, b) {
+  var r=confirm(" press ");
+  if(r==true){
+    alert("ok");
+  }
+  else{
+    alert("cancel");
+  }
+
+}
+
+function showPrompt(a, b) {
+  var name=prompt("enter your name", "Mohamed Ahmed");
+  if(name != null && name !=""){
+    document.write("Hello "+name+" Hru");
+  }
+}*/
